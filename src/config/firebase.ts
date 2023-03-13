ba17b6a01c59f1b3cd0225be888cc0c350175a45
thebowenfeng <857514.leofeng@gmail.com>
2023-03-13 16:39:55 +1100

    added firebase config files

---
+++ src/config/firebase.ts
@@ -0,0 +1,10 @@
+import {initializeApp} from "firebase/app";
+export const firebaseConfig = {
+    apiKey: "AIzaSyBL0P_D7X5WUZAnIee29F4Pt28rJabOYjk",
+    authDomain: "pennywise-c7e3f.firebaseapp.com",
+    projectId: "pennywise-c7e3f",
+    storageBucket: "pennywise-c7e3f.appspot.com",
+    messagingSenderId: "96840204912",
+    appId: "1:96840204912:web:824ff703f892398aa16ab5",
+    measurementId: "G-0Q8CWY45DD"
+};
