09562ec8c09d02d30f533da1066dd45304f6d1ea
thebowenfeng <857514.leofeng@gmail.com>
2023-03-22 11:27:13 +1100

    removed unused vars, fixed imports

--- src/components/CreateButton.tsx
+++ src/components/CreateButton.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
-import AspectRatio from "@mui/joy/AspectRatio";
 import Link from "@mui/joy/Link";
 import Card from "@mui/joy/Card";
-import Chip from "@mui/joy/Chip";
-import Typography from "@mui/joy/Typography";
 
 import Add from "@mui/icons-material/Add";
 export default function CreateButton() {
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,12 @@
 import { useUserStore } from "../../stores/user";
 import { Header } from "../../components/Header";
-import { useTheme } from "@mui/joy/styles";
-import Breadcrumbs from "../../components/Breadcrumbs";
 import ExpenseCard from "../../components/ExpenseCard";
 import Box from "@mui/material/Box";
 import Grid from "@mui/joy/Grid";
-import Button from "@mui/joy/Button";
-import ThumbUp from "@mui/icons-material/ThumbUp";
 import CreateButton from "../../components/CreateButton";
 import { useNavigate } from "react-router-dom";
 export default function Dashboard() {
   const { value } = useUserStore();
-  const theme = useTheme();
   const navigate = useNavigate();
   const handleLogout = () => {
     console.log("logout");
