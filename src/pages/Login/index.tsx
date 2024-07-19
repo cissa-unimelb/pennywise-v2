7d720386f002a51832fd29f7cdea9902c7700c0b
khangluong2004 <khangluong2004@gmail.com>
2024-07-19 21:15:49 +1000

    Remove comment

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -36,7 +36,6 @@ export default function Login() {
       .finally(() => {
         setLoading(false);
       })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
