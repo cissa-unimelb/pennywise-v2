7de199b7194288ae420698633777e115e07a9152
khangluong2004 <khangluong2004@gmail.com>
2024-08-29 12:56:40 +1000

    Fix the grid dip shit spacing

--- src/pages/Dashboard/kanbanBoard.tsx
+++ src/pages/Dashboard/kanbanBoard.tsx
@@ -11,51 +11,63 @@ type KanbanBoardProps = {
 export function KanbanBoard(props: KanbanBoardProps){
     const reimbursement = props.reimbursement;
     const user = props.user;
+
+    let active: ReimbursementRead[] = [];
+    let approve: ReimbursementRead[] = [];
+    let reject: ReimbursementRead[] = [];
+
+    reimbursement.map((reim, i) => {
+        if (reim.state === "Active"){
+            active.push(reim);
+        } else if (reim.state === "Approve"){
+            approve.push(reim);
+        } else if (reim.state === "Reject"){
+            reject.push(reim);
+        }
+        return null;
+    });
+
     return (
         <Box className="App-dashboard-container" sx={{marginTop: "100px", marginBottom: "100px"}}>
             <Grid container spacing={2}>
-                <Grid item container xs={4}>
+                <Grid item xs={4}>
                     <h1> Active </h1>
                 </Grid>
-                <Grid item container xs={4}>
+                <Grid item xs={4}>
                     <h1> Approve </h1>
                 </Grid>
-                <Grid item container xs={4}>
+                <Grid item xs={4}>
                     <h1> Reject </h1>
                 </Grid>
             </Grid>
             <Grid container spacing={2}>
-                <Grid item container xs={4} spacing={2} sx={{height: "700px", overflowY: 'scroll'}}>
+                <Grid item xs={4} spacing={2} sx={{height: "700px", overflowY: 'scroll'}}>
                 {
-                    reimbursement.map((reim, i) => (
-                    reim.state === "Active" ?
-                    <Grid item xs={12} key={i}>
+                    active.map((reim, i) => (
+                    <div className="Component-kanban-card-row">
                         <ReimbursementCard reimbursement={reim} isTreasurer={user.isTreasurer}/>
-                    </Grid>
-                    : null
+                    </div>
                     ))
                 }
                 </Grid>
-                <Grid item container xs={4} spacing={2} sx={{height: "700px", overflowY: 'scroll'}}>
+                <Grid item xs={4} spacing={2} sx={{height: "700px", overflowY: 'scroll'}}>
                 {
-                    reimbursement.map((reim, i) => (
-                    reim.state === "Approve" ?
-                    <Grid item xs={12} key={i}>
+                    approve.map((reim, i) => (
+                    <div className="Component-kanban-card-row">
                         <ReimbursementCard reimbursement={reim} isTreasurer={user.isTreasurer}/>
-                    </Grid>
-                    : null
+                    </div>
                     ))
                 }
                 </Grid>
 
-                <Grid item container xs={4} spacing={2} sx={{height: "700px", overflowY: 'scroll'}}>
+                <Grid item xs={4} 
+                    spacing={2}
+                    sx={{height: "700px", overflowY: 'scroll'}}>
                 {
-                    reimbursement.map((reim, i) => (
-                    reim.state === "Reject" ?
-                    <Grid item xs={12} key={i}>
+                    reject.map((reim, i) => (
+                    <div className="Component-kanban-card-row">
                         <ReimbursementCard reimbursement={reim} isTreasurer={user.isTreasurer}/>
-                    </Grid>
-                    : null
+                    </div>
                     ))
                 }
                 </Grid>
